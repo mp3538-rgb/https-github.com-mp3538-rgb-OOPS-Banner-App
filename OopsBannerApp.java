3fb0eb4adff9e49731421d7d497707638360a799
Meenakshi <mp3538@srmist.edu.in>
2026-03-03 11:02:46 +0530

    added UC4

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,59 +1,19 @@
-public class OopsBannerApp {
-
+public class OOPSBannerApp {
     public static void main(String[] args) {
-        printBanner();
+        String[]lines = new String[9];
+        lines[0] = String.join(" ", "    *    ", "    *    ", "**   ", "   *** ");
+        lines[1] = String.join(" ", " *     * ", " *     * ", "*    * ", " **      ");
+        lines[2] = String.join(" ", "*       *", "*       *", "*     *", "**       ");
+        lines[3] = String.join(" ", "*       *", "*       *", "*    * ", " **      ");
+        lines[4] = String.join(" ", "*       *", "*       *", "**   ", "   *   ");
+        lines[5] = String.join(" ", "*       *", "*       *", "*       ", "      * ");
+        lines[6] = String.join(" ", "*       *", "*       *", "*       ", "       *");
+        lines[7] = String.join(" ", " *    *  ", " *     * ", "*       ", "      * "); 
+        lines[8] = String.join(" ", "    *    ", "    *    ", "*       ", " **   ");
+    
+        for(String line : lines) {
+            System.out.println(line);
+       
     }
-
-    // Static method as required in UC3
-    public static void printBanner() {
-
-        System.out.println(String.join(" ",
-                " *  ",
-                " *  ",
-                " ** ",
-                " ** "
-        ));
-
-        System.out.println(String.join(" ",
-                "*   * ",
-                "*   * ",
-                "*     ",
-                "*     "
-        ));
-
-        System.out.println(String.join(" ",
-                "*   * ",
-                "*   * ",
-                "*   ",
-                " *  "
-        ));
-
-        System.out.println(String.join(" ",
-                "*   * ",
-                "*   * ",
-                "*     ",
-                "    * "
-        ));
-
-        System.out.println(String.join(" ",
-                "*   * ",
-                "*   * ",
-                "*     ",
-                "    * "
-        ));
-
-        System.out.println(String.join(" ",
-                "*   * ",
-                "*   * ",
-                "*     ",
-                "*   * "
-        ));
-
-        System.out.println(String.join(" ",
-                " *  ",
-                " *  ",
-                "*     ",
-                " *  "
-        ));
     }
 }
